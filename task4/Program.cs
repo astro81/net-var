f7ef5a55903ca9d216cce9399568ea5773573d32
astro81 <caien720@protonmail.com>
2025-11-05 12:21:45 +0545

    task4

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        int[] nums = { 1, 3, 5, 4, 8 };
+        Console.WriteLine(string.Join(", ", nums));
+
+        Array.Sort(nums);
+        Console.WriteLine(string.Join(", ", nums));
+
+        Array.Reverse(nums);
+        Console.WriteLine(string.Join(", ", nums));
+        
+        foreach (var num in nums)
+        {
+            Console.WriteLine(num);
+        }
+
+        int idx = Array.IndexOf(nums, 4);
+        Console.WriteLine(idx);
+
+    }
+}
