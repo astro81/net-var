64053b2d428f1aeae05cff8adfd8b006f4a6a2ef
astro81 <caien720@protonmail.com>
2025-11-05 12:34:05 +0545

    task6

--- task6/Program.cs
+++ task6/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> names = new List<string> { "apple", "orange", "banana" };
+        Console.WriteLine(string.Join(", ", names));
+
+        names.Add("mango");
+        Console.WriteLine(string.Join(", ", names));
+
+        names.Remove("apple");
+        Console.WriteLine(string.Join(", ", names));
+
+        foreach (var name in names)
+        {
+            Console.WriteLine(name);
+        }
+
+        Dictionary<int, string> fruits = new Dictionary<int, string>
+        {
+            { 1, "apple" },
+            { 2, "orange" },
+            { 3, "banana" }
+        };
+
+        fruits.Add(4, "mango");
+
+        Console.WriteLine("Dictionary contents:");
+        foreach (var kvp in fruits)
+        {
+            Console.WriteLine($"{kvp.Key} {kvp.Value}");
+        }
+    }
+}
